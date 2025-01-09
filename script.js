6067eb8f7784e418a6a6afe4090519c3b2868945
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-09 14:32:44 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let nombre =2;
+console.log(nombre);
+// document.querySelector() est une méthode Js qui permet de sélectionner les elements dans le DOM d'une page web.
+let titre = document.querySelector("h1");
+let btn01 =document.querySelector(".btn-1");
+let btn02 =document.querySelector(".btn-2");
+let text =document.querySelector("#text");
+let text2 =document.querySelector();
+console.log(titre);
+console.log(btn01);
+console.log(btn02);
+console.log(text);
+
+
+
+
+
+console.log(titre);
+
