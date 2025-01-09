c006bf0626be21d0d263b81340f7eebc42a64a1d
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-09 15:16:10 +0100

    ds
    ds

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 let nombre =2;
 console.log(nombre);
 // document.querySelector() est une méthode Js qui permet de sélectionner les elements dans le DOM d'une page web.
-let titre = document.querySelector("h1");
-let btn01 =document.querySelector(".btn-1");
-let btn02 =document.querySelector(".btn-2");
-let text =document.querySelector("#text");
-let text2 =document.querySelector();
-console.log(titre);
-console.log(btn01);
-console.log(btn02);
-console.log(text);
+// let titre = document.querySelector("h1");
+// let btn01 =document.querySelector(".btn-1");
+// let btn02 =document.querySelector(".btn-2");
+// let text =document.querySelector("#text");
+// let text2 =document.querySelector();
+// console.log(titre);
+// console.log(btn01);
+// console.log(btn02);
+// console.log(text);
+
+// let som = 0;
+// let a =3;
+// let b =5;
+
+const btn1 =document.querySelector("#btn-1");
+const btn2 =document.querySelector("#btn-2");
+const questionContent =document.querySelector(".click-btn");
+console.log(questionContent);
+
+// btn1.addEventListener("événement", fonction )
+// btn1.addEventListener("événement", fonction )
+
+function addition(a,b){
+  return  a + b;
+}
+let somme = addition(5,9) //appel de la fonction
+console.log(somme);
+
+ const add  =  function (a1,b1) { 
+  return a1 + b1
+}
+//   Fonction flechée
+const add2  =   (a2,b2) => {
+  return a2 + b2
+}
+const add3  =   (a3,b3) =>  a3 + b3
 
 
 
 
 
-console.log(titre);
+
+
 
