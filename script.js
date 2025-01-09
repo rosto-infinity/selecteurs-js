97cdac6a2a5ae46a619868662728143b8f4d03d4
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-09 16:26:51 +0100

    selecteurs
    selecteurs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let nombre =2;
-console.log(nombre);
+// console.log(nombre);
 // document.querySelector() est une méthode Js qui permet de sélectionner les elements dans le DOM d'une page web.
 // let titre = document.querySelector("h1");
 // let btn01 =document.querySelector(".btn-1");
@@ -18,25 +18,51 @@ console.log(nombre);
 const btn1 =document.querySelector("#btn-1");
 const btn2 =document.querySelector("#btn-2");
 const questionContent =document.querySelector(".click-btn");
-console.log(questionContent);
+const paragraph = document.querySelector('p');
+// console.log(questionContent);
+// btn1.addEventListener("événement", fonction callback )
+btn1.addEventListener("click",
+  ()=>{
+    console.log('Bafoussam'); 
+    paragraph.classList.toggle('paragraphVisible');
+    btn1.style.color = 'yellow'
+    btn1.style.borderRadius =  '2px'
+    btn1.style.padding =  '2px'
+    btn1.style.backgroundColor= 'red'
+    // btn1.style.borderRadius = 35 + 'px'
 
-// btn1.addEventListener("événement", fonction )
-// btn1.addEventListener("événement", fonction )
+  }
+ )
+btn2.addEventListener("click",
+  ()=>{
+    paragraph.classList.toggle('paragraphVisible');
+    btn2.style.backgroundColor= 'green'
+  }
+ )
+
+
+
 
 function addition(a,b){
   return  a + b;
 }
 let somme = addition(5,9) //appel de la fonction
-console.log(somme);
+// console.log(somme);
 
  const add  =  function (a1,b1) { 
   return a1 + b1
 }
-//   Fonction flechée
+// console.log(add(1,2));
+
+//   Fonction fléchée , fonction de rappel (callback), fonction anonyme
 const add2  =   (a2,b2) => {
   return a2 + b2
 }
+// console.log(add2(1,3));
+
 const add3  =   (a3,b3) =>  a3 + b3
+// console.log(add3(25,3));
+
 
 
 
